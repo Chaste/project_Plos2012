7dae1a6336615c8d5d634e29c28dc46514bd0aeb
Alex Fletcher <AlexFletcher@users.noreply.github.com>
2012-07-11 14:22:09 +0000

    [Plos2012] add 3D mesh-based spheroid example
    git-svn-id: file:///var/svn/chaste/projects/Plos2012@16124 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- test/TestSpheroidExperiments.hpp
+++ test/TestSpheroidExperiments.hpp
@@ -44,10 +44,12 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "OffLatticeSimulation.hpp"
 #include "CellBasedEventHandler.hpp"
+#include "MeshBasedCellPopulation.hpp"
 #include "NodeBasedCellPopulation.hpp"
 #include "StochasticOxygenBasedCellCycleModel.hpp"
 #include "GeneralisedLinearSpringForce.hpp"
 #include "AveragedSourcePde.hpp"
+#include "CellwiseSourcePde.hpp"
 #include "ConstBoundaryCondition.hpp"
 #include "PetscSetupAndFinalize.hpp"
 #include "CellBasedPdeHandler.hpp"
@@ -76,7 +78,7 @@ private:
 
 public:
 
-	void TestSpheroidWithPde() throw(Exception)
+    void TestSpheroidWithPde() throw(Exception)
     {
         // Create a single node
         std::vector<Node<3>* > nodes;
@@ -85,8 +87,8 @@ public:
         NodesOnlyMesh<3> mesh;
         mesh.ConstructNodesWithoutMesh(nodes);
 
-		// Create a corresponding cell
-		std::vector<CellPtr> cells;
+        // Create a corresponding cell
+        std::vector<CellPtr> cells;
 
         boost::shared_ptr<AbstractCellMutationState> p_state(new WildTypeCellMutationState);
         StochasticOxygenBasedCellCycleModel* p_model = new StochasticOxygenBasedCellCycleModel();
@@ -96,23 +98,23 @@ public:
         p_model->SetQuiescentConcentration(0.5);
         p_model->SetCriticalHypoxicDuration(2);
 
-		CellPtr p_cell(new Cell(p_state, p_model));
-		double birth_time = -RandomNumberGenerator::Instance()->ranf()*18.0;
-		p_cell->SetBirthTime(birth_time);
-		cells.push_back(p_cell);
+        CellPtr p_cell(new Cell(p_state, p_model));
+        double birth_time = -RandomNumberGenerator::Instance()->ranf()*18.0;
+        p_cell->SetBirthTime(birth_time);
+        cells.push_back(p_cell);
 
-		// Set up cell population
-		NodeBasedCellPopulation<3> cell_population(mesh, cells);
-		cell_population.SetMechanicsCutOffLength(1.5);
-		cell_population.SetAbsoluteMovementThreshold(DBL_MAX);
+        // Set up cell population
+        NodeBasedCellPopulation<3> cell_population(mesh, cells);
+        cell_population.SetMechanicsCutOffLength(1.5);
+        cell_population.SetAbsoluteMovementThreshold(DBL_MAX);
 
         // Set up cell data on the cell population
         cell_population.SetDataOnAllCells("oxygen", 1.0);
 
-		// Set up cell-based simulation
-		OffLatticeSimulation<3> simulator(cell_population);
-		simulator.SetEndTime(500);
-		simulator.SetSamplingTimestepMultiple(120);
+        // Set up cell-based simulation
+        OffLatticeSimulation<3> simulator(cell_population);
+        simulator.SetEndTime(500);
+        simulator.SetSamplingTimestepMultiple(120);
         simulator.SetOutputDirectory("Plos2012_SpheroidWithPde");
 
         // Set up PDE and pass to simulation via handler
@@ -124,28 +126,101 @@ public:
 
         CellBasedPdeHandler<3> pde_handler(&cell_population);
         pde_handler.AddPdeAndBc(&pde_and_bc);
-		simulator.SetCellBasedPdeHandler(&pde_handler);
+        simulator.SetCellBasedPdeHandler(&pde_handler);
 
         // Create a force law and pass it to the simulation
         MAKE_PTR(GeneralisedLinearSpringForce<3>, p_force);
         p_force->SetCutOffLength(1.5);
         simulator.AddForce(p_force);
 
-		// Use a coarse mesh when solving the PDE
+        // Use a coarse mesh when solving the PDE
         ChastePoint<3> lower(0.0, 0.0, 0.0);
         ChastePoint<3> upper(50.0, 50.0, 50.0);
         ChasteCuboid<3> cuboid(lower, upper);
-		pde_handler.UseCoarsePdeMesh(10.0, cuboid, true);
+        pde_handler.UseCoarsePdeMesh(10.0, cuboid, true);
 
         // Set up cell killer and pass into simulation
         MAKE_PTR_ARGS(OxygenBasedCellKiller<3>, p_killer, (&cell_population));
         simulator.AddCellKiller(p_killer);
 
-		// Solve the system
-		simulator.Solve();
+        // Solve the system
+        simulator.Solve();
 
-		CellBasedEventHandler::Headings();
-		CellBasedEventHandler::Report();
+        CellBasedEventHandler::Headings();
+        CellBasedEventHandler::Report();
+    }
+
+    void TestMeshBasedSpheroidWithPde() throw(Exception)
+    {
+        // Create a simple 3D mesh
+        std::vector<Node<3>*> nodes;
+        nodes.push_back(new Node<3>(0, true,  0.0, 0.0, 0.0));
+        nodes.push_back(new Node<3>(1, true,  1.0, 1.0, 0.0));
+        nodes.push_back(new Node<3>(2, true,  1.0, 0.0, 1.0));
+        nodes.push_back(new Node<3>(3, true,  0.0, 1.0, 1.0));
+        nodes.push_back(new Node<3>(4, false, 0.5, 0.5, 0.5));
+        MutableMesh<3,3> mesh(nodes);
+
+        // Set up cells
+        boost::shared_ptr<AbstractCellMutationState> p_state(new WildTypeCellMutationState);
+        std::vector<CellPtr> cells;
+        for (unsigned i=0; i<nodes.size(); i++)
+        {
+            StochasticOxygenBasedCellCycleModel* p_model = new StochasticOxygenBasedCellCycleModel();
+            p_model->SetDimension(3);
+            p_model->SetStemCellG1Duration(2.0);
+            p_model->SetHypoxicConcentration(0.3);
+            p_model->SetQuiescentConcentration(0.5);
+            p_model->SetCriticalHypoxicDuration(2);
+            double birth_time = -RandomNumberGenerator::Instance()->ranf()*
+                                (  p_model->GetStemCellG1Duration()
+                                 + p_model->GetSG2MDuration() );
+
+            CellPtr p_cell(new Cell(p_state, p_model));
+            p_cell->SetBirthTime(birth_time);
+            cells.push_back(p_cell);
+        }
+
+        // Create cell population
+        MeshBasedCellPopulation<3> cell_population(mesh, cells);
+        cell_population.SetAbsoluteMovementThreshold(DBL_MAX);
+        cell_population.SetOutputCellPopulationVolumes(true);
+        cell_population.SetOutputCellVolumes(true);
+        
+        // Set up cell data on the cell population
+        cell_population.SetDataOnAllCells("oxygen", 1.0);
+
+        // Set up cell-based simulation
+        OffLatticeSimulation<3> simulator(cell_population);
+        simulator.SetEndTime(500);
+        simulator.SetSamplingTimestepMultiple(120);
+        simulator.SetOutputDirectory("Plos2012_MeshBasedSpheroidWithPde");
+
+        // Set up PDE and pass to simulation via handler
+        CellwiseSourcePde<3> pde(cell_population, -1.0);
+        ConstBoundaryCondition<3> bc(1.0);
+        bool is_neumann_bc = false;
+        PdeAndBoundaryConditions<3> pde_and_bc(&pde, &bc, is_neumann_bc);
+        pde_and_bc.SetDependentVariableName("oxygen");
+
+        CellBasedPdeHandler<3> pde_handler(&cell_population);
+        pde_handler.AddPdeAndBc(&pde_and_bc);
+        simulator.SetCellBasedPdeHandler(&pde_handler);
+
+        // Create a force law and pass it to the simulation
+        MAKE_PTR(GeneralisedLinearSpringForce<3>, p_force);
+        p_force->SetCutOffLength(1.5);
+        simulator.AddForce(p_force);
+
+        // Set up cell killer and pass into simulation
+        MAKE_PTR_ARGS(OxygenBasedCellKiller<3>, p_killer, (&cell_population));
+        simulator.AddCellKiller(p_killer);
+
+        // Solve the system
+        simulator.Solve();
+
+        CellBasedEventHandler::Headings();
+        CellBasedEventHandler::Report();
     }
 };
 
